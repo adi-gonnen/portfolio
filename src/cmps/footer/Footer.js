a72dcbe6596abd8afeb72296e24107740a85629f
Adi Gonnen <adigonnen@gmail.com>
2018-11-12 21:57:56 +0200

    change cv pdf

---
+++ src/cmps/footer/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Footer.css';
+
+class Footer extends Component {
+    render() {
+        return (
+            <div className="footer flex">
+                <div>Developed by Adi Gonnen</div>
+            </div>
+        )
+    }
+}
+export default Footer
